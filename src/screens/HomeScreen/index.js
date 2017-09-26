0600452cbf0d981b8b2ba91fdc22d91d2c5edf42
Juan Riquelme González <juanriqgon@protonmail.com>
2017-09-27 00:13:13 +0200

    feat: add home screen functionality (WIP3)
    The listing of bookshelves from the API is now working.

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -2,37 +2,27 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom';
 import Bookshelf from '../../Bookshelf';
+import { shelves } from '../../utils/constants'
 
 const HomeScreen = ({ library }) => {
+  // TODO: extract this functionality to a function, maybe to an external file
+  let sortedLibrary = Object.keys(shelves).map(shelf => [shelf, []])
+  sortedLibrary = new Map(sortedLibrary)
+  library.forEach(({shelf, cover, title, authors}, id) => {
+    const bookShelf = sortedLibrary.get(shelf)
+    sortedLibrary.set(shelf, [...bookShelf, {id, cover, title, authors}])
+  });
+  const bookshelves = Array.from(sortedLibrary).map(([shelf, books]) => (
+    <Bookshelf key={shelf} shelf={shelf} books={books} />
+  ))
+
   return (
     <div className="list-books">
       <div className="list-books-title">
         <h1>MyReads</h1>
       </div>
       <div className="list-books-content">
-        <div>
-          {
-            // TODO: need to filter books of every bookshelf from library
-            console.dir(library)
-          }
-          <Bookshelf
-            shelf="currentlyReading"
-            books={[
-              {
-                id: 1,
-                cover: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
-                title: "To Kill a Mockingbird",
-                authors: ["Harper Lee"]
-              },
-              {
-                id: 2,
-                cover: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
-                title: "Ender's Game",
-                authors: ["Orson Scott Card"]
-              }
-            ]}
-          />
-        </div>
+        <div>{bookshelves}</div>
       </div>
       <div className="open-search">
         <Link to='/search'>Add a book</Link>
