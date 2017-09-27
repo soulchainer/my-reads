641c634bd5b2a0da10cb5ec4a3dac6d69948ce58
Juan Riquelme González <j.riquelme@medlabmg.com>
2017-09-27 15:54:30 +0200

    refactor: libraryToBookshelves func → a component
    Turn `libraryToBookshelves` funct to generate the bookshelves of
    our library into a component, <Bookshelves> that does just that.

---
+++ src/Bookshelves.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Bookshelf from './Bookshelf'
+import { shelves } from './utils/constants'
+
+const Bookshelves = ({library}) => {
+  let sortedLibrary = Object.keys(shelves).map(shelf => [shelf, []])
+  sortedLibrary = new Map(sortedLibrary)
+  library.forEach(({shelf, cover, title, authors}, id) => {
+    const bookShelf = sortedLibrary.get(shelf)
+    sortedLibrary.set(shelf, [...bookShelf, {id, cover, title, authors}])
+  });
+  const bookshelves = Array.from(sortedLibrary).map(([shelf, books]) => (
+    <Bookshelf key={shelf} shelf={shelf} books={books} />
+  ))
+  return <div>{bookshelves}</div>
+}
+
+export default Bookshelves
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,28 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom';
-import Bookshelf from '../../Bookshelf';
-import { shelves } from '../../utils/constants'
+import Bookshelves from '../../Bookshelves'
 
 const HomeScreen = ({ library }) => {
-  // TODO: extract this functionality to a function, maybe to an external file
-  let sortedLibrary = Object.keys(shelves).map(shelf => [shelf, []])
-  sortedLibrary = new Map(sortedLibrary)
-  library.forEach(({shelf, cover, title, authors}, id) => {
-    const bookShelf = sortedLibrary.get(shelf)
-    sortedLibrary.set(shelf, [...bookShelf, {id, cover, title, authors}])
-  });
-  const bookshelves = Array.from(sortedLibrary).map(([shelf, books]) => (
-    <Bookshelf key={shelf} shelf={shelf} books={books} />
-  ))
-
   return (
     <div className="list-books">
       <div className="list-books-title">
         <h1>MyReads</h1>
       </div>
       <div className="list-books-content">
-        <div>{bookshelves}</div>
+        <Bookshelves library={library}/>
       </div>
       <div className="open-search">
         <Link to='/search'>Add a book</Link>
