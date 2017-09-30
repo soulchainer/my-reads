17890bbec34d5745ec3d5391fb1b11fe0995c213
Juan Riquelme González <juanriqgon@protonmail.com>
2017-09-30 15:26:07 +0200

    feature: refactor SearchResults+defaultCover feat
    - Refactor `<SearchResults>` component, to get rid of `generateBook`
      function
    - Add a default cover image, for when a book doesn't have a proper
      cover

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Book from './Book';
-import { shelves } from './utils/constants';
+import { shelves, cover as defaultCover } from './utils/constants';
 
-const generateBook = (id, cover, title, authors, library, onUpdateBook) => {
-  let props = { id, cover, title, authors, onUpdateBook};
-  const fromLibrary = library.get(id)
-  if (fromLibrary) {
-    props = {...props, currentShelf: shelves[fromLibrary.shelf]};
+/**
+ * @typedef {Object} ImageLinks
+ * @property {string|undefined} smallThumbnail URL of the small thumbnail of the current book's cover
+ * @property {string|undefined} thumbnail URL of the thumbnail of the current book's cover
+ */
+
+/**
+ * If it exists, get and return the image cover of the current book.
+ * If not, return the default cover image. 
+ * @param {ImageLinks|undefined} imageLinks - An object with the thumbnails of the current book
+ * @returns {string} The fetched cover image of the current book or the default cover image
+ * @memberof SearchResults
+ */
+const getCover = (imageLinks) => {
+  let cover = defaultCover;
+  if (imageLinks) {
+    const {smallThumbnail, thumbnail} = imageLinks; 
+    cover = thumbnail ? thumbnail : smallThumbnail;
   }
-  return <Book {...props} onUpdateBook={onUpdateBook} />;
+  return cover;
 }
 
 const SearchResults = ({searchResults, library, onUpdateBook}) => {
@@ -18,14 +31,24 @@ const SearchResults = ({searchResults, library, onUpdateBook}) => {
       Array.isArray(searchResults) &&
       searchResults.map(({
         id,
-        imageLinks: {thumbnail: cover},
+        imageLinks,
         title,
         authors
-      }) => (
-        <li key={id}>
-          { generateBook(id, cover, title, authors, library, onUpdateBook) }
-        </li>
-      ))
+      }) => {
+        const inLibrary = library.get(id);
+        return (
+          <li key={id}>
+            <Book
+              id={id}
+              cover={getCover(imageLinks)}
+              title={title}
+              authors={authors}
+              currentShelf={inLibrary ? shelves[inLibrary.shelf] : shelves.none}
+              onUpdateBook={onUpdateBook}
+            />
+          </li>
+        )
+      })
     }</ul>
   );
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,10 @@
+import defaultCover from '../assets/defaultCover.svg'
+
 export const shelves = {
   'currentlyReading': 'Currently Reading',
   'wantToRead': 'Want to Read',
   'read': 'Read',
   'none': 'None'
 };
+
+export const cover = defaultCover;
